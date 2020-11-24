80d6a1546de156e3ba866b98078233c50030d194
eggtgg <73218635+eggtgg@users.noreply.github.com>
2020-11-24 15:22:29 +0700

    Update Cau9MinhTriHo.py

--- Cau9MinhTriHo.py
+++ Cau9MinhTriHo.py
@@ -3,7 +3,7 @@ import time
 a='n'
 while (a !='Y' and a!='y'):
     a= input("Tắt máy hoặc bị nguyền! (y/n): ")
-    print('lời nguyền sẽ trở lại trong 30s nữa')
+    print('lời nguyền sẽ trở lại trong 30s nữa...')
     time.sleep(30)
 print('Tắt máy!')
 os.system('shutdown -s')
